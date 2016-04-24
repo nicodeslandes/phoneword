6decc5662e142089389514e04acbaa4fdb7f02f2
Nicolas Deslandes <ndeslandes@live.fr>
2016-04-24 10:56:09 +0100

    Add project files.

---
+++ Phoneword.Windows/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Calls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Phoneword.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private string _translatedNumber;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void translateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var textNumber = numberTextBox.Text;
+            _translatedNumber = Core.PhonewordTranslator.ToNumber(textNumber);
+            if (string.IsNullOrWhiteSpace(_translatedNumber))
+            {
+                callButton.IsEnabled = false;
+                callButton.Content = "Call";
+            }
+            else
+            {
+                callButton.IsEnabled = true;
+                callButton.Content = "Call - " + _translatedNumber;
+            }
+        }
+
+        private async void callButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent(
+                "Windows.ApplicationModel.Calls.PhoneLine"))
+            {
+                var store = await PhoneCallManager.RequestStoreAsync();
+                var phoneLineId = await store.GetDefaultLineAsync();
+                var phoneLine = await PhoneLine.FromIdAsync(phoneLineId);
+                phoneLine.Dial(_translatedNumber, "");
+            }
+            else
+            {
+                var uriSkype = new Uri($"Skype:{Regex.Replace(_translatedNumber, @"[-\s]", "")}?call");
+                //var uriSkype = new Uri($"Skype:{_translatedNumber}?call");
+
+                // Set the option to show a warning
+                var promptOptions = new Windows.System.LauncherOptions {TreatAsUntrusted = false};
+
+                // Launch the URI
+                await Windows.System.Launcher.LaunchUriAsync(uriSkype, promptOptions);
+            }
+        }
+
+    }
+}
---
+++ Phoneword/MainActivity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+
+namespace Phoneword
+{
+    [Activity(Label = "Phone Word", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            var translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            var translatedNumber = string.Empty;
+
+            translateButton.Click += (s, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call " + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (s, e) =>
+            {
+                // On "Call" button click, try to dial phone number.
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate {
+                    // Create intent to dial phone
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+                    StartActivity(callIntent);
+                });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                // Show the alert dialog to the user and wait for response.
+                callDialog.Show();
+            };
+        }
+    }
+}
+
